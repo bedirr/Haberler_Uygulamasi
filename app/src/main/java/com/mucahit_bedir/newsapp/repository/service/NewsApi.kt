ba552b0b59c83521d5114c053f0d3ebdfeeca5cc
mücahit bedir <mcbedir57@gmail.com>
2022-04-11 18:48:20 +0300

    ArticleDataSource.kt, RetroFitClient.kt, NewsApi.kt, NewsResponse.kt, Util.kt dosyaları oluşturuldu.

---
+++ app/src/main/java/com/mucahit_bedir/newsapp/repository/service/NewsApi.kt
@@ -0,0 +1,24 @@
+package com.mucahit_bedir.newsapp.repository.service
+
+import android.provider.SyncStateContract
+import com.mucahit_bedir.newsapp.model.NewsResponse
+import com.mucahit_bedir.newsapp.utils.Constants
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country") country: String = "tr",
+        @Query("page") pageNumber: Int,
+        @Query("apiKey") apiKey: String = Constants.API_KEY
+    ): Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun getSearchNews(
+        @Query("q") searchQuery: String,
+        @Query("page") pageNumber: Int,
+        @Query("apiKey") apiKey: String = Constants.API_KEY
+    ): Response<NewsResponse>
+}
--- app/src/main/java/com/mucahit_bedir/newsapp/utils/Util.kt
+++ app/src/main/java/com/mucahit_bedir/newsapp/utils/Util.kt
@@ -2,26 +2,48 @@ package com.mucahit_bedir.newsapp.utils
 
 import android.content.Context
 import android.content.Intent
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.mucahit_bedir.newsapp.R
 import com.mucahit_bedir.newsapp.model.Article
 
 // share news
-fun shareNews(context: Context?, article: Article){
-    val intent = Intent().apply{
+fun shareNews(context: Context?, article: Article) {
+    val intent = Intent().apply {
         action = Intent.ACTION_SEND
         putExtra(Intent.EXTRA_TEXT, article.urlToImage)
         putExtra(Intent.EXTRA_STREAM, article.urlToImage)
         putExtra(Intent.EXTRA_TITLE, article.title)
-        type="image/*"
+        type = "image/*"
     }
     context?.startActivity(Intent.createChooser(intent, "Share News On"))
 }
-//load image in image view
-fun getCircularDrawable(context: Context):CircularProgressDrawable{
 
+//load image in image view
+fun getCircularDrawable(context: Context): CircularProgressDrawable {
     return CircularProgressDrawable(context).apply {
-        strokeWidth=8f
-        centerRadius=48f
-        setTint(context.resources.getColor(R.color.))
+        strokeWidth = 8f
+        centerRadius = 48f
+        setTint(context.resources.getColor(R.color.bgLineColor))
+        start()
+    }
+}
+
+fun ImageView.loadImage(url: String, progressDrawable: CircularProgressDrawable) {
+    val options = RequestOptions()
+        .placeholder(progressDrawable)
+        .error(R.drawable.ic_launcher_foreground)
+    Glide.with(context)
+        .setDefaultRequestOptions(options)
+        .load(url)
+        .into(this)
+}
+@BindingAdapter("loadImage")
+fun loadImage(imageView: ImageView, url: String?){
+    if (url!=null){
+        imageView.loadImage(url!!, getCircularDrawable(imageView.context))
     }
 }
